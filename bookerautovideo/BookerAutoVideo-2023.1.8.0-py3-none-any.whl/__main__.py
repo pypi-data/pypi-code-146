ee15b8f937a42f8a63f2a419bac85fa2fff3329c
Tom Forbes <tom@tomforb.es>
2023-01-07 20:20:30 +0000

    {"name":"bookerautovideo","version":"2023.1.8.0","file":"BookerAutoVideo-2023.1.8.0-py3-none-any.whl","path":"bookerautovideo/BookerAutoVideo-2023.1.8.0-py3-none-any.whl"}

---
+++ bookerautovideo/BookerAutoVideo-2023.1.8.0-py3-none-any.whl/__main__.py
@@ -0,0 +1,24 @@
+import argparse
+import sys
+import warnings
+from . import __version__
+from . import *
+
+warnings.filterwarnings("ignore")
+
+def main():
+    parser = argparse.ArgumentParser(prog="BookerAutoVideo", formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-v", "--version", action="version", version=f"PYBP version: {__version__}")
+    parser.set_defaults(func=lambda x: parser.print_help())
+    subparsers = parser.add_subparsers()
+    
+    audio2txt_parser = subparsers.add_parser("audio2txt", help="convert audio to text")
+    audio2txt_parser.add_argument("fname", help="file name")
+    audio2txt_parser.add_argument("-t", "--threads", type=int, default=8, help="num of threads")
+    audio2txt_parser.add_argument("-m", "--model", default='base', choices=['tiny', 'base', 'small', 'medium', 'large'], help="model name")
+    audio2txt_parser.set_defaults(func=audio2txt_handle)
+        
+    args = parser.parse_args()
+    args.func(args)
+    
+if __name__ == '__main__': main()
---
+++ bookerautovideo/BookerAutoVideo-2023.1.8.0-py3-none-any.whl/autovideo_config.py
@@ -0,0 +1,18 @@
+config = {
+    'name': '',
+    'format': 'mp4',
+    'font': 'kaiti.ttf',
+    'bgm': '',
+    'bgImg': '',
+    'size': [1280, 768],
+    'padding': 0,
+    'contents': [],
+    'header': '', 
+    'footer': '',
+    # 爬虫模块配置
+    'crawler': {},
+    # NovelAI 配置
+    'novelai': {},
+    # PaddleSpeech 配置
+    'tts': {},
+}
