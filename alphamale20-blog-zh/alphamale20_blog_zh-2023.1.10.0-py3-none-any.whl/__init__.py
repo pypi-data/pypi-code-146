36d6a8d89f6cddc0e2fd79f53ce77baec9c49a0b
Tom Forbes <tom@tomforb.es>
2023-01-10 15:16:37 +0000

    {"name":"alphamale20-blog-zh","version":"2023.1.10.0","file":"alphamale20_blog_zh-2023.1.10.0-py3-none-any.whl","path":"alphamale20-blog-zh/alphamale20_blog_zh-2023.1.10.0-py3-none-any.whl"}

---
+++ alphamale20-blog-zh/alphamale20_blog_zh-2023.1.10.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""alphamale20-blog-zh
+https://github.com/apachecn/alphamale20-blog-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.10.0"
