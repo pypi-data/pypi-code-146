8def15cd7f822ad7dca9ea9ab2d9cac4aeb7e711
Tom Forbes <tom@tomforb.es>
2023-01-14 10:56:01 +0000

    {"name":"jisuanji-zucheng-jiegouhua-fangfa","version":"2023.1.14.0","file":"jisuanji-zucheng-jiegouhua-fangfa-2023.1.14.0.tar.gz","path":"jisuanji-zucheng-jiegouhua-fangfa/jisuanji-zucheng-jiegouhua-fangfa-2023.1.14.0.tar.gz"}

---
+++ jisuanji-zucheng-jiegouhua-fangfa/jisuanji-zucheng-jiegouhua-fangfa-2023.1.14.0.tar.gz/JisuanjiZuchengJiegouhuaFangfa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jisuanji-zucheng-jiegouhua-fangfa
+https://github.com/apachecn/jisuanji-zucheng-jiegouhua-fangfa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
