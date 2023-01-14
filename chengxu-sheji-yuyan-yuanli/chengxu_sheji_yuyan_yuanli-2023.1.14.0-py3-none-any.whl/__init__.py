e8d37612c60a1c66c3604c9f7bb513c8529419ac
Tom Forbes <tom@tomforb.es>
2023-01-14 10:17:01 +0000

    {"name":"chengxu-sheji-yuyan-yuanli","version":"2023.1.14.0","file":"chengxu_sheji_yuyan_yuanli-2023.1.14.0-py3-none-any.whl","path":"chengxu-sheji-yuyan-yuanli/chengxu_sheji_yuyan_yuanli-2023.1.14.0-py3-none-any.whl"}

---
+++ chengxu-sheji-yuyan-yuanli/chengxu_sheji_yuyan_yuanli-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chengxu-sheji-yuyan-yuanli
+https://github.com/apachecn/chengxu-sheji-yuyan-yuanli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
