a334242a6b444cd4af497329198f2a427b42c47f
Tom Forbes <tom@tomforb.es>
2023-01-14 11:02:22 +0000

    {"name":"lisan-shuxue-jiqi-yingyong","version":"2023.1.14.0","file":"lisan-shuxue-jiqi-yingyong-2023.1.14.0.tar.gz","path":"lisan-shuxue-jiqi-yingyong/lisan-shuxue-jiqi-yingyong-2023.1.14.0.tar.gz"}

---
+++ lisan-shuxue-jiqi-yingyong/lisan-shuxue-jiqi-yingyong-2023.1.14.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import LisanShuxueJiqiYingyong
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('LisanShuxueJiqiYingyong'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="lisan-shuxue-jiqi-yingyong",
+    version=LisanShuxueJiqiYingyong.__version__,
+    url="https://github.com/apachecn/lisan-shuxue-jiqi-yingyong",
+    author=LisanShuxueJiqiYingyong.__author__,
+    author_email=LisanShuxueJiqiYingyong.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="离散数学及其应用原书第7版",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "lisan-shuxue-jiqi-yingyong=LisanShuxueJiqiYingyong.__main__:main",
+            "LisanShuxueJiqiYingyong=LisanShuxueJiqiYingyong.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
