8a1e5422d33e026430343038dadaa463eac50bf8
Tom Forbes <tom@tomforb.es>
2023-01-14 11:33:22 +0000

    {"name":"shuzi-sheji-hejisuanji-tixi-jiegou","version":"2023.1.14.0","file":"shuzi_sheji_hejisuanji_tixi_jiegou-2023.1.14.0-py3-none-any.whl","path":"shuzi-sheji-hejisuanji-tixi-jiegou/shuzi_sheji_hejisuanji_tixi_jiegou-2023.1.14.0-py3-none-any.whl"}

---
+++ shuzi-sheji-hejisuanji-tixi-jiegou/shuzi_sheji_hejisuanji_tixi_jiegou-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuzi-sheji-hejisuanji-tixi-jiegou
+https://github.com/apachecn/shuzi-sheji-hejisuanji-tixi-jiegou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
