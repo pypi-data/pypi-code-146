84673a81945525ee3983613a82b22cd6ccb68818
Tom Forbes <tom@tomforb.es>
2023-01-14 10:52:41 +0000

    {"name":"jisuanji-xitong-jiegou","version":"2023.1.14.0","file":"jisuanji_xitong_jiegou-2023.1.14.0-py3-none-any.whl","path":"jisuanji-xitong-jiegou/jisuanji_xitong_jiegou-2023.1.14.0-py3-none-any.whl"}

---
+++ jisuanji-xitong-jiegou/jisuanji_xitong_jiegou-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jisuanji-xitong-jiegou
+https://github.com/apachecn/jisuanji-xitong-jiegou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
