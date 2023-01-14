63943a8f3a4b0ed40363e76bf0710fb52fe9cc14
Tom Forbes <tom@tomforb.es>
2023-01-14 10:28:27 +0000

    {"name":"gaoji-bianyiqi-sheji-yushixian","version":"2023.1.14.0","file":"gaoji_bianyiqi_sheji_yushixian-2023.1.14.0-py3-none-any.whl","path":"gaoji-bianyiqi-sheji-yushixian/gaoji_bianyiqi_sheji_yushixian-2023.1.14.0-py3-none-any.whl"}

---
+++ gaoji-bianyiqi-sheji-yushixian/gaoji_bianyiqi_sheji_yushixian-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gaoji-bianyiqi-sheji-yushixian
+https://github.com/apachecn/gaoji-bianyiqi-sheji-yushixian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
