1ac925c8ce061da93f327cd5151228b3fcc3b54d
Tom Forbes <tom@tomforb.es>
2023-01-14 10:15:43 +0000

    {"name":"caozuo-xitong-jingsui-yusheji-yuanli","version":"2023.1.14.0","file":"caozuo_xitong_jingsui_yusheji_yuanli-2023.1.14.0-py3-none-any.whl","path":"caozuo-xitong-jingsui-yusheji-yuanli/caozuo_xitong_jingsui_yusheji_yuanli-2023.1.14.0-py3-none-any.whl"}

---
+++ caozuo-xitong-jingsui-yusheji-yuanli/caozuo_xitong_jingsui_yusheji_yuanli-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""caozuo-xitong-jingsui-yusheji-yuanli
+https://github.com/apachecn/caozuo-xitong-jingsui-yusheji-yuanli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
