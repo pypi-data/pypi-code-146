0ee3e7c90b646dbbff38c3abf2e21dc3acb6fd99
Tom Forbes <tom@tomforb.es>
2023-01-14 11:21:24 +0000

    {"name":"shuju-jiegou-suanfa-yuyingyong-cpp-yuyan-miaoshu","version":"2023.1.14.0","file":"shuju-jiegou-suanfa-yuyingyong-cpp-yuyan-miaoshu-2023.1.14.0.tar.gz","path":"shuju-jiegou-suanfa-yuyingyong-cpp-yuyan-miaoshu/shuju-jiegou-suanfa-yuyingyong-cpp-yuyan-miaoshu-2023.1.14.0.tar.gz"}

---
+++ shuju-jiegou-suanfa-yuyingyong-cpp-yuyan-miaoshu/shuju-jiegou-suanfa-yuyingyong-cpp-yuyan-miaoshu-2023.1.14.0.tar.gz/ShujuJiegouSuanfaYuyingyongCppYuyanMiaoshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuju-jiegou-suanfa-yuyingyong-cpp-yuyan-miaoshu
+https://github.com/apachecn/shuju-jiegou-suanfa-yuyingyong-cpp-yuyan-miaoshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
