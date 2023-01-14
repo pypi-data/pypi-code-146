08ebdf68deb50dde992aa9d5eda134f98939af7c
Tom Forbes <tom@tomforb.es>
2023-01-14 11:05:58 +0000

    {"name":"ml-chengxu-sheji-jiaocheng","version":"2023.1.14.0","file":"ml-chengxu-sheji-jiaocheng-2023.1.14.0.tar.gz","path":"ml-chengxu-sheji-jiaocheng/ml-chengxu-sheji-jiaocheng-2023.1.14.0.tar.gz"}

---
+++ ml-chengxu-sheji-jiaocheng/ml-chengxu-sheji-jiaocheng-2023.1.14.0.tar.gz/MlChengxuShejiJiaocheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ml-chengxu-sheji-jiaocheng
+https://github.com/apachecn/ml-chengxu-sheji-jiaocheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
