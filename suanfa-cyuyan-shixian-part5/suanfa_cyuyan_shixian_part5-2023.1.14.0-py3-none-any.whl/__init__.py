833a7e49712e6e63a9d730684d0207cca36b46c8
Tom Forbes <tom@tomforb.es>
2023-01-14 11:40:43 +0000

    {"name":"suanfa-cyuyan-shixian-part5","version":"2023.1.14.0","file":"suanfa_cyuyan_shixian_part5-2023.1.14.0-py3-none-any.whl","path":"suanfa-cyuyan-shixian-part5/suanfa_cyuyan_shixian_part5-2023.1.14.0-py3-none-any.whl"}

---
+++ suanfa-cyuyan-shixian-part5/suanfa_cyuyan_shixian_part5-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""suanfa-cyuyan-shixian-part5
+https://github.com/apachecn/suanfa-cyuyan-shixian-part5"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
