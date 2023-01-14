4ead1efda3385e3bf9e2618321d6a6b0b11f64d1
Tom Forbes <tom@tomforb.es>
2023-01-14 11:31:29 +0000

    {"name":"shujuku-xitong-shixian","version":"2023.1.14.0","file":"shujuku-xitong-shixian-2023.1.14.0.tar.gz","path":"shujuku-xitong-shixian/shujuku-xitong-shixian-2023.1.14.0.tar.gz"}

---
+++ shujuku-xitong-shixian/shujuku-xitong-shixian-2023.1.14.0.tar.gz/ShujukuXitongShixian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shujuku-xitong-shixian
+https://github.com/apachecn/shujuku-xitong-shixian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
