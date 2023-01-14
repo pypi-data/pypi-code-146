40070ede726b2f2aab7d7f07b10edb74eff9593f
Tom Forbes <tom@tomforb.es>
2023-01-14 11:46:44 +0000

    {"name":"x3d-jisuanji-tuxingxue","version":"2023.1.14.0","file":"x3d_jisuanji_tuxingxue-2023.1.14.0-py3-none-any.whl","path":"x3d-jisuanji-tuxingxue/x3d_jisuanji_tuxingxue-2023.1.14.0-py3-none-any.whl"}

---
+++ x3d-jisuanji-tuxingxue/x3d_jisuanji_tuxingxue-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x3d-jisuanji-tuxingxue
+https://github.com/apachecn/x3d-jisuanji-tuxingxue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
