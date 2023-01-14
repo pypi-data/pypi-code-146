2aebf43b7f2cce5335afcdaac8c78b4b61c398a2
Tom Forbes <tom@tomforb.es>
2023-01-14 11:13:16 +0000

    {"name":"ruanjian-gongcheng-jiagou-qudongde-ruanjian-kaifa","version":"2023.1.14.0","file":"ruanjian-gongcheng-jiagou-qudongde-ruanjian-kaifa-2023.1.14.0.tar.gz","path":"ruanjian-gongcheng-jiagou-qudongde-ruanjian-kaifa/ruanjian-gongcheng-jiagou-qudongde-ruanjian-kaifa-2023.1.14.0.tar.gz"}

---
+++ ruanjian-gongcheng-jiagou-qudongde-ruanjian-kaifa/ruanjian-gongcheng-jiagou-qudongde-ruanjian-kaifa-2023.1.14.0.tar.gz/RuanjianGongchengJiagouQudongdeRuanjianKaifa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruanjian-gongcheng-jiagou-qudongde-ruanjian-kaifa
+https://github.com/apachecn/ruanjian-gongcheng-jiagou-qudongde-ruanjian-kaifa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
