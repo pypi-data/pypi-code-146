912645cbba4fd945ced812103045bca8fbdbd388
Tom Forbes <tom@tomforb.es>
2023-01-14 11:41:12 +0000

    {"name":"suanfa-daolun-huazhang","version":"2023.1.14.0","file":"suanfa_daolun_huazhang-2023.1.14.0-py3-none-any.whl","path":"suanfa-daolun-huazhang/suanfa_daolun_huazhang-2023.1.14.0-py3-none-any.whl"}

---
+++ suanfa-daolun-huazhang/suanfa_daolun_huazhang-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""suanfa-daolun-huazhang
+https://github.com/apachecn/suanfa-daolun-huazhang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
