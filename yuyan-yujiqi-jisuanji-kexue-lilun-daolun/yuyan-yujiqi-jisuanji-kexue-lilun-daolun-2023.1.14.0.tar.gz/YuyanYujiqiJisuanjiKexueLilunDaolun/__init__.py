538cea37b8440645b81abe87b8fb208da221488a
Tom Forbes <tom@tomforb.es>
2023-01-14 11:58:33 +0000

    {"name":"yuyan-yujiqi-jisuanji-kexue-lilun-daolun","version":"2023.1.14.0","file":"yuyan-yujiqi-jisuanji-kexue-lilun-daolun-2023.1.14.0.tar.gz","path":"yuyan-yujiqi-jisuanji-kexue-lilun-daolun/yuyan-yujiqi-jisuanji-kexue-lilun-daolun-2023.1.14.0.tar.gz"}

---
+++ yuyan-yujiqi-jisuanji-kexue-lilun-daolun/yuyan-yujiqi-jisuanji-kexue-lilun-daolun-2023.1.14.0.tar.gz/YuyanYujiqiJisuanjiKexueLilunDaolun/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yuyan-yujiqi-jisuanji-kexue-lilun-daolun
+https://github.com/apachecn/yuyan-yujiqi-jisuanji-kexue-lilun-daolun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
