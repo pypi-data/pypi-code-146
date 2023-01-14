8660e345142f53324f9168725bcf36c266f62b02
Tom Forbes <tom@tomforb.es>
2023-01-14 11:01:55 +0000

    {"name":"lisan-shuxue-jiqi-yingyong","version":"2023.1.14.0","file":"lisan_shuxue_jiqi_yingyong-2023.1.14.0-py3-none-any.whl","path":"lisan-shuxue-jiqi-yingyong/lisan_shuxue_jiqi_yingyong-2023.1.14.0-py3-none-any.whl"}

---
+++ lisan-shuxue-jiqi-yingyong/lisan_shuxue_jiqi_yingyong-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lisan-shuxue-jiqi-yingyong
+https://github.com/apachecn/lisan-shuxue-jiqi-yingyong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
