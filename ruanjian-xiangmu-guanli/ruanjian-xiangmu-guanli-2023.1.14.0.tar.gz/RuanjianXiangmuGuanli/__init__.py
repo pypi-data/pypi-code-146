17e68c2e2946f05f9d38451a53a862e776d95e9e
Tom Forbes <tom@tomforb.es>
2023-01-14 11:14:58 +0000

    {"name":"ruanjian-xiangmu-guanli","version":"2023.1.14.0","file":"ruanjian-xiangmu-guanli-2023.1.14.0.tar.gz","path":"ruanjian-xiangmu-guanli/ruanjian-xiangmu-guanli-2023.1.14.0.tar.gz"}

---
+++ ruanjian-xiangmu-guanli/ruanjian-xiangmu-guanli-2023.1.14.0.tar.gz/RuanjianXiangmuGuanli/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruanjian-xiangmu-guanli
+https://github.com/apachecn/ruanjian-xiangmu-guanli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
