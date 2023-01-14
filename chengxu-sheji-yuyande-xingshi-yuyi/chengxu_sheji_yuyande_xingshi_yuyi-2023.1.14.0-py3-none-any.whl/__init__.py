645bc42b4c474f4e52a40e31c1ea0712668712ff
Tom Forbes <tom@tomforb.es>
2023-01-14 10:17:41 +0000

    {"name":"chengxu-sheji-yuyande-xingshi-yuyi","version":"2023.1.14.0","file":"chengxu_sheji_yuyande_xingshi_yuyi-2023.1.14.0-py3-none-any.whl","path":"chengxu-sheji-yuyande-xingshi-yuyi/chengxu_sheji_yuyande_xingshi_yuyi-2023.1.14.0-py3-none-any.whl"}

---
+++ chengxu-sheji-yuyande-xingshi-yuyi/chengxu_sheji_yuyande_xingshi_yuyi-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chengxu-sheji-yuyande-xingshi-yuyi
+https://github.com/apachecn/chengxu-sheji-yuyande-xingshi-yuyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
