d6b6ce27425c87e748cc14af721f7be16fe2f754
Tom Forbes <tom@tomforb.es>
2023-01-14 10:52:13 +0000

    {"name":"jisuanji-xitong-hexin-gainian-jiruanyingjian-shixian","version":"2023.1.14.0","file":"jisuanji_xitong_hexin_gainian_jiruanyingjian_shixian-2023.1.14.0-py3-none-any.whl","path":"jisuanji-xitong-hexin-gainian-jiruanyingjian-shixian/jisuanji_xitong_hexin_gainian_jiruanyingjian_shixian-2023.1.14.0-py3-none-any.whl"}

---
+++ jisuanji-xitong-hexin-gainian-jiruanyingjian-shixian/jisuanji_xitong_hexin_gainian_jiruanyingjian_shixian-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jisuanji-xitong-hexin-gainian-jiruanyingjian-shixian
+https://github.com/apachecn/jisuanji-xitong-hexin-gainian-jiruanyingjian-shixian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
