f267403045c16709045bb7a6932ce526de332f6b
Tom Forbes <tom@tomforb.es>
2023-01-14 10:23:15 +0000

    {"name":"duochuliqi-bianchengde-yishu","version":"2023.1.14.0","file":"duochuliqi_bianchengde_yishu-2023.1.14.0-py3-none-any.whl","path":"duochuliqi-bianchengde-yishu/duochuliqi_bianchengde_yishu-2023.1.14.0-py3-none-any.whl"}

---
+++ duochuliqi-bianchengde-yishu/duochuliqi_bianchengde_yishu-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""duochuliqi-bianchengde-yishu
+https://github.com/apachecn/duochuliqi-bianchengde-yishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
