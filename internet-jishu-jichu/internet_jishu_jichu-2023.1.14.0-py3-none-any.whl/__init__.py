4da763a7f48c609ac2eaecb57adf48992b080ced
Tom Forbes <tom@tomforb.es>
2023-01-14 10:32:25 +0000

    {"name":"internet-jishu-jichu","version":"2023.1.14.0","file":"internet_jishu_jichu-2023.1.14.0-py3-none-any.whl","path":"internet-jishu-jichu/internet_jishu_jichu-2023.1.14.0-py3-none-any.whl"}

---
+++ internet-jishu-jichu/internet_jishu_jichu-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""internet-jishu-jichu
+https://github.com/apachecn/internet-jishu-jichu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
