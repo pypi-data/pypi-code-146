72d1c34c427b93b13e3415b1f237f7b233f8e787
Tom Forbes <tom@tomforb.es>
2023-01-14 10:53:17 +0000

    {"name":"jisuanji-xitong-xitong-jiagou","version":"2023.1.14.0","file":"jisuanji_xitong_xitong_jiagou-2023.1.14.0-py3-none-any.whl","path":"jisuanji-xitong-xitong-jiagou/jisuanji_xitong_xitong_jiagou-2023.1.14.0-py3-none-any.whl"}

---
+++ jisuanji-xitong-xitong-jiagou/jisuanji_xitong_xitong_jiagou-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jisuanji-xitong-xitong-jiagou
+https://github.com/apachecn/jisuanji-xitong-xitong-jiagou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
