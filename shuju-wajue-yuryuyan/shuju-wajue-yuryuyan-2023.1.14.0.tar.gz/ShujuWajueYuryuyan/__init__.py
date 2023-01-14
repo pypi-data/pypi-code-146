915d0e02eefdeee287617d8a0382c45de84d57d3
Tom Forbes <tom@tomforb.es>
2023-01-14 11:26:38 +0000

    {"name":"shuju-wajue-yuryuyan","version":"2023.1.14.0","file":"shuju-wajue-yuryuyan-2023.1.14.0.tar.gz","path":"shuju-wajue-yuryuyan/shuju-wajue-yuryuyan-2023.1.14.0.tar.gz"}

---
+++ shuju-wajue-yuryuyan/shuju-wajue-yuryuyan-2023.1.14.0.tar.gz/ShujuWajueYuryuyan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuju-wajue-yuryuyan
+https://github.com/apachecn/shuju-wajue-yuryuyan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
