3c0aa549bc1a36c927e95d3f6402028824871649
Tom Forbes <tom@tomforb.es>
2023-01-14 10:25:52 +0000

    {"name":"fenbushi-aozuo-xitong-yuanli-yushixian","version":"2023.1.14.0","file":"fenbushi-aozuo-xitong-yuanli-yushixian-2023.1.14.0.tar.gz","path":"fenbushi-aozuo-xitong-yuanli-yushixian/fenbushi-aozuo-xitong-yuanli-yushixian-2023.1.14.0.tar.gz"}

---
+++ fenbushi-aozuo-xitong-yuanli-yushixian/fenbushi-aozuo-xitong-yuanli-yushixian-2023.1.14.0.tar.gz/FenbushiAozuoXitongYuanliYushixian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fenbushi-aozuo-xitong-yuanli-yushixian
+https://github.com/apachecn/fenbushi-aozuo-xitong-yuanli-yushixian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
