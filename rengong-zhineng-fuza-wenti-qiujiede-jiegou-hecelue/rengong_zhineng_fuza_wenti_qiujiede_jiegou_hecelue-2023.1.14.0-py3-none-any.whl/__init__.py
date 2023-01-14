d9d912911a173410f3aacfdb2ef1ff374192e9de
Tom Forbes <tom@tomforb.es>
2023-01-14 11:07:43 +0000

    {"name":"rengong-zhineng-fuza-wenti-qiujiede-jiegou-hecelue","version":"2023.1.14.0","file":"rengong_zhineng_fuza_wenti_qiujiede_jiegou_hecelue-2023.1.14.0-py3-none-any.whl","path":"rengong-zhineng-fuza-wenti-qiujiede-jiegou-hecelue/rengong_zhineng_fuza_wenti_qiujiede_jiegou_hecelue-2023.1.14.0-py3-none-any.whl"}

---
+++ rengong-zhineng-fuza-wenti-qiujiede-jiegou-hecelue/rengong_zhineng_fuza_wenti_qiujiede_jiegou_hecelue-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""rengong-zhineng-fuza-wenti-qiujiede-jiegou-hecelue
+https://github.com/apachecn/rengong-zhineng-fuza-wenti-qiujiede-jiegou-hecelue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
