2457fc49dcc8cf70153f43809093a35438ca9aa1
Tom Forbes <tom@tomforb.es>
2023-01-14 11:20:57 +0000

    {"name":"shuju-jicheng-yuanli","version":"2023.1.14.0","file":"shuju-jicheng-yuanli-2023.1.14.0.tar.gz","path":"shuju-jicheng-yuanli/shuju-jicheng-yuanli-2023.1.14.0.tar.gz"}

---
+++ shuju-jicheng-yuanli/shuju-jicheng-yuanli-2023.1.14.0.tar.gz/ShujuJichengYuanli/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuju-jicheng-yuanli
+https://github.com/apachecn/shuju-jicheng-yuanli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
