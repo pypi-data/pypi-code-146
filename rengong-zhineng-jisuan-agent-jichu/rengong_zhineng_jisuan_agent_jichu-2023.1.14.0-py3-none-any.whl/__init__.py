044089fd4f9ddbc1d1c187e1e6349a50f3a93980
Tom Forbes <tom@tomforb.es>
2023-01-14 11:08:25 +0000

    {"name":"rengong-zhineng-jisuan-agent-jichu","version":"2023.1.14.0","file":"rengong_zhineng_jisuan_agent_jichu-2023.1.14.0-py3-none-any.whl","path":"rengong-zhineng-jisuan-agent-jichu/rengong_zhineng_jisuan_agent_jichu-2023.1.14.0-py3-none-any.whl"}

---
+++ rengong-zhineng-jisuan-agent-jichu/rengong_zhineng_jisuan_agent_jichu-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""rengong-zhineng-jisuan-agent-jichu
+https://github.com/apachecn/rengong-zhineng-jisuan-agent-jichu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
