00f0821f2fb5ee4e29b17771737fe7205b5fef55
Tom Forbes <tom@tomforb.es>
2023-01-14 11:59:06 +0000

    {"name":"zhuanjia-xitong-yuanli-yubiancheng","version":"2023.1.14.0","file":"zhuanjia-xitong-yuanli-yubiancheng-2023.1.14.0.tar.gz","path":"zhuanjia-xitong-yuanli-yubiancheng/zhuanjia-xitong-yuanli-yubiancheng-2023.1.14.0.tar.gz"}

---
+++ zhuanjia-xitong-yuanli-yubiancheng/zhuanjia-xitong-yuanli-yubiancheng-2023.1.14.0.tar.gz/ZhuanjiaXitongYuanliYubiancheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhuanjia-xitong-yuanli-yubiancheng
+https://github.com/apachecn/zhuanjia-xitong-yuanli-yubiancheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
