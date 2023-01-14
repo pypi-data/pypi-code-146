9a760948343724551a5e1c5b74a3a7574b4d5c30
Tom Forbes <tom@tomforb.es>
2023-01-14 10:32:47 +0000

    {"name":"jingyan-ruanjian-gongcheng","version":"2023.1.14.0","file":"jingyan-ruanjian-gongcheng-2023.1.14.0.tar.gz","path":"jingyan-ruanjian-gongcheng/jingyan-ruanjian-gongcheng-2023.1.14.0.tar.gz"}

---
+++ jingyan-ruanjian-gongcheng/jingyan-ruanjian-gongcheng-2023.1.14.0.tar.gz/JingyanRuanjianGongcheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingyan-ruanjian-gongcheng
+https://github.com/apachecn/jingyan-ruanjian-gongcheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
