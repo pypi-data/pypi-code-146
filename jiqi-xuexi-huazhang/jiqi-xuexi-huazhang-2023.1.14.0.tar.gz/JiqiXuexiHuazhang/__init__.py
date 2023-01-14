2a2e6d6a8a93210c3b80dbd5ef5ddf92ea3b0f26
Tom Forbes <tom@tomforb.es>
2023-01-14 10:34:45 +0000

    {"name":"jiqi-xuexi-huazhang","version":"2023.1.14.0","file":"jiqi-xuexi-huazhang-2023.1.14.0.tar.gz","path":"jiqi-xuexi-huazhang/jiqi-xuexi-huazhang-2023.1.14.0.tar.gz"}

---
+++ jiqi-xuexi-huazhang/jiqi-xuexi-huazhang-2023.1.14.0.tar.gz/JiqiXuexiHuazhang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiqi-xuexi-huazhang
+https://github.com/apachecn/jiqi-xuexi-huazhang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
