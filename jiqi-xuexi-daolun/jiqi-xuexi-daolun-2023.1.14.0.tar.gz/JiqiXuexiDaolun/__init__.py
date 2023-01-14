0aa975a9827544a4f7e2ade441ae949e0f7d9f9c
Tom Forbes <tom@tomforb.es>
2023-01-14 10:34:25 +0000

    {"name":"jiqi-xuexi-daolun","version":"2023.1.14.0","file":"jiqi-xuexi-daolun-2023.1.14.0.tar.gz","path":"jiqi-xuexi-daolun/jiqi-xuexi-daolun-2023.1.14.0.tar.gz"}

---
+++ jiqi-xuexi-daolun/jiqi-xuexi-daolun-2023.1.14.0.tar.gz/JiqiXuexiDaolun/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiqi-xuexi-daolun
+https://github.com/apachecn/jiqi-xuexi-daolun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
