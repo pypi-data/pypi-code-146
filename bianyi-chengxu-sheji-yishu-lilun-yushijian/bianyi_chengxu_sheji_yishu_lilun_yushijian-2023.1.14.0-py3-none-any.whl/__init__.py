c92aef3317f06e01af83cead26cb4f7a852d6b3a
Tom Forbes <tom@tomforb.es>
2023-01-14 10:08:00 +0000

    {"name":"bianyi-chengxu-sheji-yishu-lilun-yushijian","version":"2023.1.14.0","file":"bianyi_chengxu_sheji_yishu_lilun_yushijian-2023.1.14.0-py3-none-any.whl","path":"bianyi-chengxu-sheji-yishu-lilun-yushijian/bianyi_chengxu_sheji_yishu_lilun_yushijian-2023.1.14.0-py3-none-any.whl"}

---
+++ bianyi-chengxu-sheji-yishu-lilun-yushijian/bianyi_chengxu_sheji_yishu_lilun_yushijian-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bianyi-chengxu-sheji-yishu-lilun-yushijian
+https://github.com/apachecn/bianyi-chengxu-sheji-yishu-lilun-yushijian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
