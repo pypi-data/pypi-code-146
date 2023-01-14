c9e7762ba0bd226506f4e8a0d51424dec9ef4a7d
Tom Forbes <tom@tomforb.es>
2023-01-14 11:46:21 +0000

    {"name":"wuxian-chuanganqi-wangluo-yuanli","version":"2023.1.14.0","file":"wuxian_chuanganqi_wangluo_yuanli-2023.1.14.0-py3-none-any.whl","path":"wuxian-chuanganqi-wangluo-yuanli/wuxian_chuanganqi_wangluo_yuanli-2023.1.14.0-py3-none-any.whl"}

---
+++ wuxian-chuanganqi-wangluo-yuanli/wuxian_chuanganqi_wangluo_yuanli-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wuxian-chuanganqi-wangluo-yuanli
+https://github.com/apachecn/wuxian-chuanganqi-wangluo-yuanli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
