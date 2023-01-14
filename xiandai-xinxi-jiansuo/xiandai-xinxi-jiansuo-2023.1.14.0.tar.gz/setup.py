c6472ad33efca76ad2cd564634b2bae6686b64f5
Tom Forbes <tom@tomforb.es>
2023-01-14 11:50:13 +0000

    {"name":"xiandai-xinxi-jiansuo","version":"2023.1.14.0","file":"xiandai-xinxi-jiansuo-2023.1.14.0.tar.gz","path":"xiandai-xinxi-jiansuo/xiandai-xinxi-jiansuo-2023.1.14.0.tar.gz"}

---
+++ xiandai-xinxi-jiansuo/xiandai-xinxi-jiansuo-2023.1.14.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import XiandaiXinxiJiansuo
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('XiandaiXinxiJiansuo'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="xiandai-xinxi-jiansuo",
+    version=XiandaiXinxiJiansuo.__version__,
+    url="https://github.com/apachecn/xiandai-xinxi-jiansuo",
+    author=XiandaiXinxiJiansuo.__author__,
+    author_email=XiandaiXinxiJiansuo.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="现代信息检索原书第2版",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "xiandai-xinxi-jiansuo=XiandaiXinxiJiansuo.__main__:main",
+            "XiandaiXinxiJiansuo=XiandaiXinxiJiansuo.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
