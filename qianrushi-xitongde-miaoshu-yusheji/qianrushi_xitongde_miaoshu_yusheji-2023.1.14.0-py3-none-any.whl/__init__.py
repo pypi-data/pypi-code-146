979d69968d554b8e443b4e7f80ec55296c30b80c
Tom Forbes <tom@tomforb.es>
2023-01-14 11:07:09 +0000

    {"name":"qianrushi-xitongde-miaoshu-yusheji","version":"2023.1.14.0","file":"qianrushi_xitongde_miaoshu_yusheji-2023.1.14.0-py3-none-any.whl","path":"qianrushi-xitongde-miaoshu-yusheji/qianrushi_xitongde_miaoshu_yusheji-2023.1.14.0-py3-none-any.whl"}

---
+++ qianrushi-xitongde-miaoshu-yusheji/qianrushi_xitongde_miaoshu_yusheji-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qianrushi-xitongde-miaoshu-yusheji
+https://github.com/apachecn/qianrushi-xitongde-miaoshu-yusheji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
