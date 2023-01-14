0d10eae09a4fb7474a4b523e178943aa8ba71866
Tom Forbes <tom@tomforb.es>
2023-01-14 10:09:35 +0000

    {"name":"bianyi-yuanli-huazhang","version":"2023.1.14.0","file":"bianyi-yuanli-huazhang-2023.1.14.0.tar.gz","path":"bianyi-yuanli-huazhang/bianyi-yuanli-huazhang-2023.1.14.0.tar.gz"}

---
+++ bianyi-yuanli-huazhang/bianyi-yuanli-huazhang-2023.1.14.0.tar.gz/BianyiYuanliHuazhang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bianyi-yuanli-huazhang
+https://github.com/apachecn/bianyi-yuanli-huazhang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
