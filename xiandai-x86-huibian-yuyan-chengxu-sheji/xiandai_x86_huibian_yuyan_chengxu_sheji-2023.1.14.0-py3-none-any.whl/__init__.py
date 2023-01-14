b4890c8628ddf77cca56e03003d85e23cc875929
Tom Forbes <tom@tomforb.es>
2023-01-14 11:49:27 +0000

    {"name":"xiandai-x86-huibian-yuyan-chengxu-sheji","version":"2023.1.14.0","file":"xiandai_x86_huibian_yuyan_chengxu_sheji-2023.1.14.0-py3-none-any.whl","path":"xiandai-x86-huibian-yuyan-chengxu-sheji/xiandai_x86_huibian_yuyan_chengxu_sheji-2023.1.14.0-py3-none-any.whl"}

---
+++ xiandai-x86-huibian-yuyan-chengxu-sheji/xiandai_x86_huibian_yuyan_chengxu_sheji-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiandai-x86-huibian-yuyan-chengxu-sheji
+https://github.com/apachecn/xiandai-x86-huibian-yuyan-chengxu-sheji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
