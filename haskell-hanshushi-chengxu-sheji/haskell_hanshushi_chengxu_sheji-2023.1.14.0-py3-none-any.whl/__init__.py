7e3ee3b363cef5c53737ae81c0a894c0c6788a70
Tom Forbes <tom@tomforb.es>
2023-01-14 10:30:24 +0000

    {"name":"haskell-hanshushi-chengxu-sheji","version":"2023.1.14.0","file":"haskell_hanshushi_chengxu_sheji-2023.1.14.0-py3-none-any.whl","path":"haskell-hanshushi-chengxu-sheji/haskell_hanshushi_chengxu_sheji-2023.1.14.0-py3-none-any.whl"}

---
+++ haskell-hanshushi-chengxu-sheji/haskell_hanshushi_chengxu_sheji-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""haskell-hanshushi-chengxu-sheji
+https://github.com/apachecn/haskell-hanshushi-chengxu-sheji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
