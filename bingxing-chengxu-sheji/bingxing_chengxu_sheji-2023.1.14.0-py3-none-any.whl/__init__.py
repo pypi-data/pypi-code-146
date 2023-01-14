d05e8445b45abf8000241170c454b30533c4ef5f
Tom Forbes <tom@tomforb.es>
2023-01-14 10:12:19 +0000

    {"name":"bingxing-chengxu-sheji","version":"2023.1.14.0","file":"bingxing_chengxu_sheji-2023.1.14.0-py3-none-any.whl","path":"bingxing-chengxu-sheji/bingxing_chengxu_sheji-2023.1.14.0-py3-none-any.whl"}

---
+++ bingxing-chengxu-sheji/bingxing_chengxu_sheji-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bingxing-chengxu-sheji
+https://github.com/apachecn/bingxing-chengxu-sheji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
