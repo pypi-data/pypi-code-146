f6eb0ce9c52e2bb9483bd513be856b2b600db8b3
Tom Forbes <tom@tomforb.es>
2023-01-14 11:02:57 +0000

    {"name":"mianxiang-duixiang-ruanjian-gongcheng","version":"2023.1.14.0","file":"mianxiang_duixiang_ruanjian_gongcheng-2023.1.14.0-py3-none-any.whl","path":"mianxiang-duixiang-ruanjian-gongcheng/mianxiang_duixiang_ruanjian_gongcheng-2023.1.14.0-py3-none-any.whl"}

---
+++ mianxiang-duixiang-ruanjian-gongcheng/mianxiang_duixiang_ruanjian_gongcheng-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mianxiang-duixiang-ruanjian-gongcheng
+https://github.com/apachecn/mianxiang-duixiang-ruanjian-gongcheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
