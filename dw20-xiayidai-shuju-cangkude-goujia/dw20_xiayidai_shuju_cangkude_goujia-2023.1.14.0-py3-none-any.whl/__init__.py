eb0f008a59f3620fbf8ef7ce6056aea1d18f3a8c
Tom Forbes <tom@tomforb.es>
2023-01-14 10:24:45 +0000

    {"name":"dw20-xiayidai-shuju-cangkude-goujia","version":"2023.1.14.0","file":"dw20_xiayidai_shuju_cangkude_goujia-2023.1.14.0-py3-none-any.whl","path":"dw20-xiayidai-shuju-cangkude-goujia/dw20_xiayidai_shuju_cangkude_goujia-2023.1.14.0-py3-none-any.whl"}

---
+++ dw20-xiayidai-shuju-cangkude-goujia/dw20_xiayidai_shuju_cangkude_goujia-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""dw20-xiayidai-shuju-cangkude-goujia
+https://github.com/apachecn/dw20-xiayidai-shuju-cangkude-goujia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
