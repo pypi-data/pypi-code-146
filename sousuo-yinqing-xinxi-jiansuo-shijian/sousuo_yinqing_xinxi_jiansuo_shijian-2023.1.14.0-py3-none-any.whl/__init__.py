3d0c02d56844fb86f1326d83706ec54532dce01b
Tom Forbes <tom@tomforb.es>
2023-01-14 11:33:59 +0000

    {"name":"sousuo-yinqing-xinxi-jiansuo-shijian","version":"2023.1.14.0","file":"sousuo_yinqing_xinxi_jiansuo_shijian-2023.1.14.0-py3-none-any.whl","path":"sousuo-yinqing-xinxi-jiansuo-shijian/sousuo_yinqing_xinxi_jiansuo_shijian-2023.1.14.0-py3-none-any.whl"}

---
+++ sousuo-yinqing-xinxi-jiansuo-shijian/sousuo_yinqing_xinxi_jiansuo_shijian-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sousuo-yinqing-xinxi-jiansuo-shijian
+https://github.com/apachecn/sousuo-yinqing-xinxi-jiansuo-shijian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
