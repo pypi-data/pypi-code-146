ab19b54001a24bb20d0d9015e46955de556a0e03
Tom Forbes <tom@tomforb.es>
2023-01-14 11:09:23 +0000

    {"name":"ruanjian-anquan-congyuantou-kaishi","version":"2023.1.14.0","file":"ruanjian-anquan-congyuantou-kaishi-2023.1.14.0.tar.gz","path":"ruanjian-anquan-congyuantou-kaishi/ruanjian-anquan-congyuantou-kaishi-2023.1.14.0.tar.gz"}

---
+++ ruanjian-anquan-congyuantou-kaishi/ruanjian-anquan-congyuantou-kaishi-2023.1.14.0.tar.gz/RuanjianAnquanCongyuantouKaishi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruanjian-anquan-congyuantou-kaishi
+https://github.com/apachecn/ruanjian-anquan-congyuantou-kaishi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
