df8892fa07dad54528c58f73c8e4a4edc029cc6d
Tom Forbes <tom@tomforb.es>
2023-01-14 11:47:57 +0000

    {"name":"x80x86-huibian-yuyan-yujisuanji-tixi-jiegou","version":"2023.1.14.0","file":"x80x86_huibian_yuyan_yujisuanji_tixi_jiegou-2023.1.14.0-py3-none-any.whl","path":"x80x86-huibian-yuyan-yujisuanji-tixi-jiegou/x80x86_huibian_yuyan_yujisuanji_tixi_jiegou-2023.1.14.0-py3-none-any.whl"}

---
+++ x80x86-huibian-yuyan-yujisuanji-tixi-jiegou/x80x86_huibian_yuyan_yujisuanji_tixi_jiegou-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""x80x86-huibian-yuyan-yujisuanji-tixi-jiegou
+https://github.com/apachecn/x80x86-huibian-yuyan-yujisuanji-tixi-jiegou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
