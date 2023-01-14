cffd8cb50d590b780fa8b8bb80877fd629b0fd2c
Tom Forbes <tom@tomforb.es>
2023-01-14 11:42:46 +0000

    {"name":"tcpip-xiangjie-vol1","version":"2023.1.14.0","file":"tcpip_xiangjie_vol1-2023.1.14.0-py3-none-any.whl","path":"tcpip-xiangjie-vol1/tcpip_xiangjie_vol1-2023.1.14.0-py3-none-any.whl"}

---
+++ tcpip-xiangjie-vol1/tcpip_xiangjie_vol1-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tcpip-xiangjie-vol1
+https://github.com/apachecn/tcpip-xiangjie-vol1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
