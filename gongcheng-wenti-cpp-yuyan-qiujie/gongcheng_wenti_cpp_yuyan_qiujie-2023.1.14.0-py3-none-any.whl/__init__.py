84117e7a3c28e658498e53a09a526ad34cef7c7d
Tom Forbes <tom@tomforb.es>
2023-01-14 10:29:28 +0000

    {"name":"gongcheng-wenti-cpp-yuyan-qiujie","version":"2023.1.14.0","file":"gongcheng_wenti_cpp_yuyan_qiujie-2023.1.14.0-py3-none-any.whl","path":"gongcheng-wenti-cpp-yuyan-qiujie/gongcheng_wenti_cpp_yuyan_qiujie-2023.1.14.0-py3-none-any.whl"}

---
+++ gongcheng-wenti-cpp-yuyan-qiujie/gongcheng_wenti_cpp_yuyan_qiujie-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gongcheng-wenti-cpp-yuyan-qiujie
+https://github.com/apachecn/gongcheng-wenti-cpp-yuyan-qiujie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
