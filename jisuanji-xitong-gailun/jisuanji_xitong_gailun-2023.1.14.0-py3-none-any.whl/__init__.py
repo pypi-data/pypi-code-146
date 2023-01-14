190956bc8e2e37c3b5f125ce3e203315abd22511
Tom Forbes <tom@tomforb.es>
2023-01-14 10:51:44 +0000

    {"name":"jisuanji-xitong-gailun","version":"2023.1.14.0","file":"jisuanji_xitong_gailun-2023.1.14.0-py3-none-any.whl","path":"jisuanji-xitong-gailun/jisuanji_xitong_gailun-2023.1.14.0-py3-none-any.whl"}

---
+++ jisuanji-xitong-gailun/jisuanji_xitong_gailun-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jisuanji-xitong-gailun
+https://github.com/apachecn/jisuanji-xitong-gailun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
