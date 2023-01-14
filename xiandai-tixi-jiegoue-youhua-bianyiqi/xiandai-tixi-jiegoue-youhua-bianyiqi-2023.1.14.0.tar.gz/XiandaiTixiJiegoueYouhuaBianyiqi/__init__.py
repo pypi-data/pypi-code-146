142c9d2db1b7d1b392c19df285fe5c38dc67d360
Tom Forbes <tom@tomforb.es>
2023-01-14 11:49:13 +0000

    {"name":"xiandai-tixi-jiegoue-youhua-bianyiqi","version":"2023.1.14.0","file":"xiandai-tixi-jiegoue-youhua-bianyiqi-2023.1.14.0.tar.gz","path":"xiandai-tixi-jiegoue-youhua-bianyiqi/xiandai-tixi-jiegoue-youhua-bianyiqi-2023.1.14.0.tar.gz"}

---
+++ xiandai-tixi-jiegoue-youhua-bianyiqi/xiandai-tixi-jiegoue-youhua-bianyiqi-2023.1.14.0.tar.gz/XiandaiTixiJiegoueYouhuaBianyiqi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiandai-tixi-jiegoue-youhua-bianyiqi
+https://github.com/apachecn/xiandai-tixi-jiegoue-youhua-bianyiqi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
