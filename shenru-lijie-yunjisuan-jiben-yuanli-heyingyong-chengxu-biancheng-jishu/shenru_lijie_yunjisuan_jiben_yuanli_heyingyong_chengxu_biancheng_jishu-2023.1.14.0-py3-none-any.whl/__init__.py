1e62b66dd970e5e4618886143c91f696b7d7d2d8
Tom Forbes <tom@tomforb.es>
2023-01-14 11:19:56 +0000

    {"name":"shenru-lijie-yunjisuan-jiben-yuanli-heyingyong-chengxu-biancheng-jishu","version":"2023.1.14.0","file":"shenru_lijie_yunjisuan_jiben_yuanli_heyingyong_chengxu_biancheng_jishu-2023.1.14.0-py3-none-any.whl","path":"shenru-lijie-yunjisuan-jiben-yuanli-heyingyong-chengxu-biancheng-jishu/shenru_lijie_yunjisuan_jiben_yuanli_heyingyong_chengxu_biancheng_jishu-2023.1.14.0-py3-none-any.whl"}

---
+++ shenru-lijie-yunjisuan-jiben-yuanli-heyingyong-chengxu-biancheng-jishu/shenru_lijie_yunjisuan_jiben_yuanli_heyingyong_chengxu_biancheng_jishu-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenru-lijie-yunjisuan-jiben-yuanli-heyingyong-chengxu-biancheng-jishu
+https://github.com/apachecn/shenru-lijie-yunjisuan-jiben-yuanli-heyingyong-chengxu-biancheng-jishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
