a8ae093bc5b8c431f4c16c3e2f95d6f4b53d4792
Tom Forbes <tom@tomforb.es>
2023-01-14 11:04:38 +0000

    {"name":"mianxiang-jisuhanji-kexuede-shuli-luoji-xitong-jianmo-yutuili","version":"2023.1.14.0","file":"mianxiang-jisuhanji-kexuede-shuli-luoji-xitong-jianmo-yutuili-2023.1.14.0.tar.gz","path":"mianxiang-jisuhanji-kexuede-shuli-luoji-xitong-jianmo-yutuili/mianxiang-jisuhanji-kexuede-shuli-luoji-xitong-jianmo-yutuili-2023.1.14.0.tar.gz"}

---
+++ mianxiang-jisuhanji-kexuede-shuli-luoji-xitong-jianmo-yutuili/mianxiang-jisuhanji-kexuede-shuli-luoji-xitong-jianmo-yutuili-2023.1.14.0.tar.gz/MianxiangJisuhanjiKexuedeShuliLuojiXitongJianmoYutuili/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mianxiang-jisuhanji-kexuede-shuli-luoji-xitong-jianmo-yutuili
+https://github.com/apachecn/mianxiang-jisuhanji-kexuede-shuli-luoji-xitong-jianmo-yutuili"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
