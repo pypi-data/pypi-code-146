95abf7a2f585c6e7f103f19cab1f3c1435e25000
Tom Forbes <tom@tomforb.es>
2023-01-14 11:33:39 +0000

    {"name":"shuzi-tuxiang-chuli-yinan-jiexi","version":"2023.1.14.0","file":"shuzi_tuxiang_chuli_yinan_jiexi-2023.1.14.0-py3-none-any.whl","path":"shuzi-tuxiang-chuli-yinan-jiexi/shuzi_tuxiang_chuli_yinan_jiexi-2023.1.14.0-py3-none-any.whl"}

---
+++ shuzi-tuxiang-chuli-yinan-jiexi/shuzi_tuxiang_chuli_yinan_jiexi-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuzi-tuxiang-chuli-yinan-jiexi
+https://github.com/apachecn/shuzi-tuxiang-chuli-yinan-jiexi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
