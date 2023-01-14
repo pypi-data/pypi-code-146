ab7dd385a8ceaab0bac776c9dce11df03ebc3e06
Tom Forbes <tom@tomforb.es>
2023-01-14 11:06:46 +0000

    {"name":"python-yuyan-chengxu-sheji","version":"2023.1.14.0","file":"python_yuyan_chengxu_sheji-2023.1.14.0-py3-none-any.whl","path":"python-yuyan-chengxu-sheji/python_yuyan_chengxu_sheji-2023.1.14.0-py3-none-any.whl"}

---
+++ python-yuyan-chengxu-sheji/python_yuyan_chengxu_sheji-2023.1.14.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""python-yuyan-chengxu-sheji
+https://github.com/apachecn/python-yuyan-chengxu-sheji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.14.0"
